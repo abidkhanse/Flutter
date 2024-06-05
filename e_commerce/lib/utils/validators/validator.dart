5354c0e8900cd04e95721d49d037d80ec3200479
abid khan <abidkhan303@hotmai.com>
2024-06-05 20:02:14 +0200

    flutter splash screen is implemented

---
+++ e_commerce/lib/utils/validators/validator.dart
@@ -0,0 +1,67 @@
+import 'package:get_storage/get_storage.dart';
+
+class TValidator {
+
+  TValidator._();
+
+  static String? validateEmail(String? email) {
+
+    if(email == null || email.isEmpty) {
+      return 'E-mail is required';
+    }
+
+    final emailRegEx = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    if(!emailRegEx.hasMatch(email)) {
+      return 'invalid e-mail address';
+    }
+
+    return null;
+
+  }
+
+  static String? validatePassword(String? password) {
+
+    if(password == null || password.isEmpty) {
+      return 'Password is required';
+    }
+
+    if(password.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+
+    if(!password.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must have at least 1 upper case letter';
+    }
+
+    if(!password.contains(RegExp(r'[0-9]'))) {
+      return 'Password must have at least 1 digit';
+    }
+
+    if(!password.contains(RegExp(r'[!@#$^&*(),.?":{}|<>]'))) {
+      return 'Password must have at least 1 special character';
+    }
+
+    return null;
+
+  }
+
+  static String? validatePhoneNumber(String? phone) {
+
+    if(phone == null || phone.isEmpty) {
+      return 'Phone is required';
+    }
+
+    final phoneRegEx = RegExp(r'^\d(10)$');
+
+    if(!phoneRegEx.hasMatch(phone)) {
+      return 'invalid phone number format , 10 digits at least';
+    }
+
+    return null;
+
+  }
+
+
+
+}
